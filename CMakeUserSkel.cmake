47a03e6ef39423eb035cd92480361d03ae95b9f9
Markus Diem <diemmarkus@gmail.com>
2017-07-04 16:35:50 +0200

    lcov added to travis.yml

--- CMakeUserSkel.cmake
+++ CMakeUserSkel.cmake
@@ -4,14 +4,14 @@
 
 IF (CMAKE_CL_64)
 
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/Qt/qt-everywhere-opensource-src-5.8.0-x64/qtbase/bin/")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/coding/3rd-party/OpenCV/build2017-x64")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/coding/READ/nomacs/build2017-x64")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/Qt/qt-everywhere-opensource-src-5.9.1-x64/qtbase/bin/")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/3rd-party/OpenCV/build2017-x64")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/READ/nomacs/build2017-x64")
 	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "../ReadFramework/build2017-x64")
 ELSE ()
 	
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/Qt/qt-everywhere-opensource-src-5.8.0-x86/qtbase/bin/")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/coding/3rd-party/OpenCV/build2017-x86")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/coding/READ/nomacs/build2017-x86")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/Qt/qt-everywhere-opensource-src-5.9.1-x86/qtbase/bin/")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/3rd-party/OpenCV/build2017-x86")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/READ/nomacs/build2017-x86")
 	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "../ReadFramework/build2017-x64")
 ENDIF ()
