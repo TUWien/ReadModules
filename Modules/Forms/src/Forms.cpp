bbe6b26fa3b40908b139fba5f744bd47d7e17f91
Florian Kleber <kleber@caa.tuwien.ac.at>
2017-07-28 16:44:46 +0200

    debugging

--- Modules/Forms/src/Forms.cpp
+++ Modules/Forms/src/Forms.cpp
@@ -373,22 +373,27 @@ QSharedPointer<nmc::DkImageContainer> FormsAnalysis::runPlugin(
 
 			qDebug() << "Match template...";
 			formF.matchTemplate();
-												
+				
 
 			resultImg = formF.drawLinesNotUsedForm(drawImg);
 			cv::cvtColor(resultImg, resultImg, CV_BGR2RGBA);
 			result = rdf::Image::mat2QImage(resultImg);
 			imgC->setImage(result, "Lines not used");
 
-			resultImg = formF.drawMaxClique(drawImg,10, 2);
+			resultImg = formF.drawLines(drawImg);
 			cv::cvtColor(resultImg, resultImg, CV_BGR2RGBA);
 			result = rdf::Image::mat2QImage(resultImg);
-			imgC->setImage(result, "maxClique 2");
+			imgC->setImage(result, "Lines used");
 
-			resultImg = formF.drawMaxClique(drawImg, 10, 1);
-			cv::cvtColor(resultImg, resultImg, CV_BGR2RGBA);
-			result = rdf::Image::mat2QImage(resultImg);
-			imgC->setImage(result, "maxClique 1");
+			//resultImg = formF.drawMaxClique(drawImg,10, 2);
+			//cv::cvtColor(resultImg, resultImg, CV_BGR2RGBA);
+			//result = rdf::Image::mat2QImage(resultImg);
+			//imgC->setImage(result, "maxClique 2");
+
+			//resultImg = formF.drawMaxClique(drawImg, 10, 1);
+			//cv::cvtColor(resultImg, resultImg, CV_BGR2RGBA);
+			//result = rdf::Image::mat2QImage(resultImg);
+			//imgC->setImage(result, "maxClique 1");
 
 			resultImg = formF.drawMaxClique(drawImg);
 			cv::cvtColor(resultImg, resultImg, CV_BGR2RGBA);
