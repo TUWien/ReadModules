eb4c8a25ea57b0ff54979d080d11f7aeeb69eb79
Markus Diem <diemmarkus@gmail.com>
2018-06-12 18:22:47 +0200

    DeepMerge plugin added

---
+++ Modules/DeepMerge/src/DeepMergePlugin.cpp
@@ -0,0 +1,273 @@
+/*******************************************************************************************************
+ReadModules are plugins for nomacs developed at CVL/TU Wien for the EU project READ. 
+
+Copyright (C) 2016 Markus Diem <diem@caa.tuwien.ac.at>
+Copyright (C) 2016 Stefan Fiel <fiel@caa.tuwien.ac.at>
+Copyright (C) 2016 Florian Kleber <kleber@caa.tuwien.ac.at>
+
+This file is part of ReadModules.
+
+ReadFramework is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ReadFramework is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+The READ project  has  received  funding  from  the European  Union’s  Horizon  2020  
+research  and innovation programme under grant agreement No 674943
+
+related links:
+[1] http://www.caa.tuwien.ac.at/cvl/
+[2] https://transkribus.eu/Transkribus/
+[3] https://github.com/TUWien/
+[4] http://nomacs.org
+*******************************************************************************************************/
+
+#include "DeepMergePlugin.h"
+
+// ReadFramework
+#include "Settings.h"
+#include "Utils.h"
+#include "DeepMerge.h"
+
+// nomacs
+#include "DkImageStorage.h"
+#include "DkSettings.h"
+
+#pragma warning(push, 0)	// no warnings from includes - begin
+#include <QAction>
+#include <QUuid>
+#include <opencv2/ml.hpp>
+
+#include <QLabel>
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+
+#pragma warning(pop)		// no warnings from includes - end
+
+namespace rdm {
+
+/**
+*	Constructor
+**/
+DeepMergePlugin::DeepMergePlugin(QObject* parent) : QObject(parent) {
+
+	// create run IDs
+	QVector<QString> runIds;
+	runIds.resize(id_end);
+
+	for (QString& rid : runIds)
+		rid = QUuid::createUuid().toString();
+
+	mRunIDs = runIds.toList();
+	
+	// create menu actions
+	QVector<QString> menuNames;
+	menuNames.resize(id_end);
+
+	menuNames[id_graph_cut]			= tr("Merge Garph Cut");
+	menuNames[id_threshold]			= tr("Merge Threshold");
+	mMenuNames = menuNames.toList();
+
+	// create menu status tips
+	QVector<QString> statusTips;
+	statusTips.resize(id_end);
+
+	statusTips[id_graph_cut]		= tr("Uses a MRF for merging the tensors");
+	statusTips[id_threshold]		= tr("Uses a simple threshold for merging the tensors");
+	mMenuStatusTips = statusTips.toList();
+
+	// saved default settings
+	rdf::DefaultSettings s;
+	s.beginGroup(name());
+
+	DeepMergeConfig dc;
+	dc.saveDefaultSettings(s);
+	
+	s.endGroup();
+}
+/**
+*	Destructor
+**/
+DeepMergePlugin::~DeepMergePlugin() {
+}
+
+void DeepMergePlugin::postLoadPlugin(const QVector<QSharedPointer<nmc::DkBatchInfo> >& batchInfo) const {
+
+	// add batch actions here...
+}
+
+QString DeepMergePlugin::settingsFilePath() const {
+	return rdf::Config::instance().settingsFilePath();
+}
+
+void DeepMergePlugin::saveSettings(QSettings & settings) const {
+
+	settings.beginGroup(name());
+	mConfig.saveSettings(settings);
+	settings.endGroup();
+}
+
+void DeepMergePlugin::loadSettings(QSettings & settings) {
+
+	// update settings
+	settings.beginGroup(name());
+	mConfig.loadSettings(settings);
+	settings.endGroup();
+}
+
+QString DeepMergePlugin::name() const {
+	return "DeepMergePlugin";
+}
+
+/**
+* Returns descriptive iamge for every ID
+* @param plugin ID
+**/
+QImage DeepMergePlugin::image() const {
+
+	return QImage(":/LayoutPlugin/img/read.png");
+};
+
+QList<QAction*> DeepMergePlugin::createActions(QWidget* parent) {
+
+	if (mActions.empty()) {
+
+		for (int idx = 0; idx < id_end; idx++) {
+			QAction* ca = new QAction(mMenuNames[idx], parent);
+			ca->setObjectName(mMenuNames[idx]);
+			ca->setStatusTip(mMenuStatusTips[idx]);
+			ca->setData(mRunIDs[idx]);	// runID needed for calling function runPlugin()
+			mActions.append(ca);
+		}
+	}
+
+	return mActions;
+}
+
+QList<QAction*> DeepMergePlugin::pluginActions() const {
+	return mActions;
+}
+
+/**
+* Main function: runs plugin based on its ID
+* @param plugin ID
+* @param image to be processed
+**/
+QSharedPointer<nmc::DkImageContainer> DeepMergePlugin::runPlugin(
+	const QString &runID, 
+	QSharedPointer<nmc::DkImageContainer> imgC, 
+	const nmc::DkSaveInfo& saveInfo,
+	QSharedPointer<nmc::DkBatchInfo>& batchInfo) const {
+
+	//// load suplemental XML
+	//QString loadXmlPath = rdf::PageXmlParser::imagePathToXmlPath(saveInfo.inputFilePath());
+	//rdf::PageXmlParser parser;
+	//parser.read(loadXmlPath);
+
+	//// set our header info
+	//auto xmlPage = parser.page();
+	//xmlPage->setCreator(QString("CVL"));
+	//xmlPage->setImageSize(QSize(imgC->image().size()));
+	//xmlPage->setImageFileName(imgC->fileName());
+
+	cv::Mat rImg;
+
+	if(runID == mRunIDs[id_graph_cut]) {
+
+		cv::Mat imgCv = nmc::DkImage::qImage2Mat(imgC->image());
+		rImg = compute(imgCv);
+
+	}
+	else if (runID == mRunIDs[id_threshold]) {
+
+		cv::Mat imgCv = nmc::DkImage::qImage2Mat(imgC->image());
+
+		// compute layout analysis
+		rdf::DeepMerge dm(imgCv);
+
+
+		rImg = dm.thresh(imgCv, 100);
+	}
+	 
+	if (!rImg.empty()) {
+
+		if (rImg.channels() == 1)
+			cv::cvtColor(rImg, rImg, cv::COLOR_GRAY2RGB);
+		QImage img = nmc::DkImage::mat2QImage(rImg);
+		imgC->setImage(img, tr("DeepMerge Visualized"));
+	}
+
+	//// save xml
+	//if (mConfig.saveXml()) {
+	//	QString saveXmlPath = rdf::PageXmlParser::imagePathToXmlPath(saveInfo.outputFilePath());
+	//	
+	//	if (saveXmlPath.isEmpty()) {
+	//		saveXmlPath = rdf::Utils::createFilePath(rdf::PageXmlParser::imagePathToXmlPath(imgC->filePath()), "-results");
+	//	}
+	//	
+	//	parser.write(saveXmlPath, parser.page());
+	//}
+
+	// wrong runID? - do nothing
+	return imgC;
+}
+
+cv::Mat DeepMergePlugin::compute(const cv::Mat & src) const {
+
+
+	rdf::Timer dt;
+
+	cv::Mat img = src.clone();
+
+	// compute layout analysis
+	rdf::DeepMerge dm(img);
+	//la.setConfig(QSharedPointer<rdf::LayoutAnalysisConfig>(new rdf::LayoutAnalysisConfig(mLAConfig)));
+
+	if (!dm.compute())
+		qWarning() << "could not compute DeepMerge...";
+
+	return dm.image();
+}
+
+// configurations that are specific for the plugin --------------------------------------------------------------------
+DeepMergeConfig::DeepMergeConfig() : ModuleConfig("General") {
+}
+
+QString DeepMergeConfig::toString() const {
+
+	QString msg = rdf::ModuleConfig::toString();
+	msg += drawResults() ? " drawing results\n" : " not drawing results\n";
+
+	return msg;
+}
+
+bool DeepMergeConfig::drawResults() const {
+	return mDrawResults;
+}
+
+bool DeepMergeConfig::saveXml() const {
+	return mSaveXml;
+}
+
+void DeepMergeConfig::load(const QSettings & settings) {
+
+	mDrawResults = settings.value("drawResults", mDrawResults).toBool();
+	mSaveXml = settings.value("saveXml", mSaveXml).toBool();
+}
+
+void DeepMergeConfig::save(QSettings & settings) const {
+
+	settings.setValue("drawResults", mDrawResults);
+	settings.setValue("saveXml", mSaveXml);
+}
+
+};
+
---
+++ Modules/DeepMerge/src/DeepMergePlugin.h
@@ -0,0 +1,114 @@
+/*******************************************************************************************************
+ReadModules are plugins for nomacs developed at CVL/TU Wien for the EU project READ. 
+
+Copyright (C) 2016 Markus Diem <diem@caa.tuwien.ac.at>
+Copyright (C) 2016 Stefan Fiel <fiel@caa.tuwien.ac.at>
+Copyright (C) 2016 Florian Kleber <kleber@caa.tuwien.ac.at>
+
+This file is part of ReadModules.
+
+ReadFramework is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ReadFramework is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+The READ project  has  received  funding  from  the European  Union’s  Horizon  2020  
+research  and innovation programme under grant agreement No 674943
+
+related links:
+[1] http://www.caa.tuwien.ac.at/cvl/
+[2] https://transkribus.eu/Transkribus/
+[3] https://github.com/TUWien/
+[4] http://nomacs.org
+*******************************************************************************************************/
+
+#pragma once
+
+#include "DkPluginInterface.h"
+#include "BaseModule.h"
+
+#pragma warning(push, 0)	// no warnings from includes - begin
+#include <QDialog>
+#pragma warning(pop)		// no warnings from includes - end
+
+// opencv defines
+namespace cv {
+	class Mat;
+}
+
+namespace rdm {
+
+class DeepMergeConfig : public rdf::ModuleConfig {
+
+public:
+	DeepMergeConfig();
+	~DeepMergeConfig() {};
+
+	virtual QString toString() const override;
+
+	bool drawResults() const;
+	bool saveXml() const;
+
+protected:
+	
+	bool mDrawResults = false;
+	bool mSaveXml = true;
+
+	void load(const QSettings& settings) override;
+	void save(QSettings& settings) const override;
+};
+
+class DeepMergePlugin : public QObject, nmc::DkBatchPluginInterface {
+	Q_OBJECT
+		Q_INTERFACES(nmc::DkBatchPluginInterface)
+		Q_PLUGIN_METADATA(IID "com.nomacs.ImageLounge.DeepMerge/1.0" FILE "DeepMergePlugin.json")
+
+public:
+	DeepMergePlugin(QObject* parent = 0);
+	~DeepMergePlugin();
+
+	QImage image() const override;
+
+	QList<QAction*> createActions(QWidget* parent) override;
+	QList<QAction*> pluginActions() const override;
+	QSharedPointer<nmc::DkImageContainer> runPlugin(
+		const QString &runID, 
+		QSharedPointer<nmc::DkImageContainer> imgC, 
+		const nmc::DkSaveInfo& saveInfo,
+		QSharedPointer<nmc::DkBatchInfo>& batchInfo) const override;
+
+	virtual void preLoadPlugin() const override {};
+	virtual void postLoadPlugin(const QVector<QSharedPointer<nmc::DkBatchInfo> > &) const override;
+	
+	// settings
+	virtual QString settingsFilePath() const override;
+	virtual void saveSettings(QSettings& settings) const override;
+	virtual void loadSettings(QSettings& settings) override;
+	virtual QString name() const override;
+
+	enum {
+		id_graph_cut,
+		id_threshold,
+
+		id_end
+	};
+
+protected:
+	QList<QAction*> mActions;
+	QStringList mRunIDs;
+	QStringList mMenuNames;
+	QStringList mMenuStatusTips;
+	DeepMergeConfig mConfig;
+
+	// layout plugin functions
+	cv::Mat compute(const cv::Mat& src) const;
+};
+};
